b4437eb31dd1d3ca13b3daecfb379043336794cc
Вячеслав Титов <vcslvttv@gmail.com>
2020-07-25 01:41:13 +0500

    Scene Manager

---
+++ WarFly/SceneManager.swift
@@ -0,0 +1,15 @@
+//
+//  SceneManager.swift
+//  WarFly
+//
+//  Created by vcslvttv on 25/07/2020.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+
+class SceneManager {
+    static let shared = SceneManager()
+    
+    var gameScene: GameScene?
+}
+
